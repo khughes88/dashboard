14ca2178d3144ef0c6c1661cd644e3198c46e85d
Keith Hughes <khughes88@gmail.com>
2015-09-02 12:56:26 +0000

    nitrous config

--- mailer.rb
+++ mailer.rb
@@ -4,11 +4,11 @@ require 'net/smtp'
 def mailer(subj,msg)
 
 #recips=['keith.hughes@citi.com']
-recips=['keith.hughes@citi.com','paud.okeeffe@citi.com','david.joseph.thompson@citi.com','dave.fleming@citi.com']
+  recips=['khughes88@gmail.com']
 
 recips.each{|recip|
 message = <<MESSAGE_END
-From: CIL QC Dashboard <do not reply>
+From: QC Dashboard <do not reply>
 To: <#{recip}>
 Subject: #{subj}
 #{msg}
@@ -17,7 +17,7 @@ MESSAGE_END
 Net::SMTP.start('mailhub-vip.ny.ssmb.com') do |smtp|
   smtp.send_message message, recip, recip
 end
-	
+
 }
 
 end
